c0940e1d9728218fa1036bcaa28398bff0d60818
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-07-24 17:13:22 -0400

    Projeto final

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.sobremesa import Sobremesa
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_mexicano = Restaurante("Mexican Food", "Mexicana")
-restaurante_mexicano.receber_avaliacao("Paulo", 5)
-restaurante_mexicano.receber_avaliacao("Miguel", 3)
-restaurante_mexicano.receber_avaliacao("Bentes", 5)
+bebida_suco = Bebida("Suco de Melancia", 5.0, "Grande")
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato("Pãozinho", 2.0, "O melhor pãozinho da cidade")
+prato_paozinho.aplicar_desconto()
+sobremesa_pudimzinho = Sobremesa("Pudimzinho", 12.0, "Doce", "Grande", "Um baita pudim, mas você terá que competir com a arlequina")
+sobremesa_pudimzinho.aplicar_desconto()
+restaurante_mexicano.adicionar_no_cardapio(bebida_suco)
+restaurante_mexicano.adicionar_no_cardapio(prato_paozinho)
+restaurante_mexicano.adicionar_no_cardapio(sobremesa_pudimzinho)
 
 def main():
-  Restaurante.listar_restaurantes()
+  restaurante_mexicano.listar_cardapio()
 
 if __name__ == "__main__":
   main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,8 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.item_cardapio import ItemCardapio
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 class Restaurante:
   restaurantes = []
@@ -8,17 +12,18 @@ class Restaurante:
     self._categoria = categoria
     self._ativo = False 
     self._avaliacao = []
+    self._cardapio = []
     Restaurante.restaurantes.append(self)
 
-  def __str__(self):
+  def __str__(self): # Imprime esse valor se for chamado como texto
     return f"\nNome: {self._nome} \nCategoria: {self._categoria}"
   
-  @classmethod
+  @classmethod # Uma função é declarada como classmethod quando é preciso chamar a classe na lógica da função
   def listar_restaurantes(cls):
     for restaurante in cls.restaurantes:
       print(f"\nNome: {restaurante._nome} \nCategoria: {restaurante._categoria} \nAtivo: {restaurante.ativo} \nAvaliação: {restaurante.media_avaliacoes}")
   
-  @property
+  @property # Uma função é declarada como propriedade quando o retorno é um valor para ser usado no código
   def ativo(self):
     return '✔️' if self._ativo else '❌'
 
@@ -40,4 +45,21 @@ class Restaurante:
       avaliacao = Avaliacao(cliente, nota)
       self._avaliacao.append(avaliacao)
 
-Restaurante.listar_restaurantes()
+  def adicionar_no_cardapio(self, item):
+    if isinstance(item, ItemCardapio):
+      self._cardapio.append(item)
+
+  def listar_cardapio(self):
+    print(f"Cardápio do restaurante {self._nome}\n")
+    for i, item in enumerate(self._cardapio, start=1):
+      mensagem = f"{i}. Nome: {item._nome} | Preço: R$ {item._preco}"
+      tipo_item = type(item)
+      
+      if tipo_item == Sobremesa:
+        mensagem += f" | Tipo: {item._tipo} | Tamanho: {item._tamanho} | Descrição: {item._descricao}"
+      elif tipo_item == Bebida:
+        mensagem += f" | Tamanho: {item._tamanho}"
+      elif tipo_item == Prato:
+        mensagem += f" | Descrição: {item._descricao}"
+
+      print(mensagem)
