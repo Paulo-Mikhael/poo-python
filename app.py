1ef20b3ae9f952fded8e4332ce3c7776d632b3bf
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-07-24 18:03:38 -0400

    Chamando API

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 from modelos.restaurante import Restaurante
 from modelos.cardapio.sobremesa import Sobremesa
 from modelos.cardapio.bebida import Bebida
@@ -14,8 +17,35 @@ restaurante_mexicano.adicionar_no_cardapio(bebida_suco)
 restaurante_mexicano.adicionar_no_cardapio(prato_paozinho)
 restaurante_mexicano.adicionar_no_cardapio(sobremesa_pudimzinho)
 
+def chamar_api():
+  url = "https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json"
+  response = requests.get(url)
+  dados_do_restaurante = {}
+
+  if response.status_code == 200:
+    dados_json = response.json()
+
+    for item in dados_json:
+      nome_do_restaurante = item["Company"]
+      if nome_do_restaurante not in dados_do_restaurante:
+        dados_do_restaurante[nome_do_restaurante] = []
+      dados_do_restaurante[nome_do_restaurante].append({
+        "item": item["Item"],
+        "price": item["price"],
+        "description": item["description"]
+      })
+    
+    print(response)
+  else:
+    print(f"Ocorreu o erro de número {response.status_code}")
+
+  for nome_restaurante, dados in dados_do_restaurante.items():
+    nome_do_arquivo = f"{nome_restaurante}.json"
+    with open(nome_do_arquivo, "w") as arquivo_restaurante:
+      json.dump(dados, arquivo_restaurante, indent=2)
+
 def main():
-  restaurante_mexicano.listar_cardapio()
+  chamar_api()
 
 if __name__ == "__main__":
   main()
